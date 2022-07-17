34b1678353180b6282daadf14a04da147a4e33e1
tonyfroes <tonydanilofroes16@gmail.com>
2022-07-17 16:57:48 -0300

    Imovel feito

--- Imovel/Imovel.py
+++ Imovel/Imovel.py
@@ -4,67 +4,160 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
 
-#Variaveis
-#Idade do imovel, área, distancia do centro, valor_condominioo do condominio
+# Variaveis
+# Idade do imovel, área, distancia do centro e valor condominio
 
-idade_do_imovelInput = 0
-areaInput = 0
-distancia_centroInput = 0
-#valor_condominio = 0
+IDADE_IMOVEL_INPUT = 0
+AREA_INPUT = 0
+DISTANCIA_CENTRO_INPUT = 0
+VALOR_CONDOMINIO = 0
 
-#Definindo as variaveis de entrada e saida do sistema fuzzy
-idade_do_imovel = ctrl.Antecedent(np.arange(0, 80, 1), 'idade_do_imovel')
-area =  ctrl.Antecedent(np.arange(12, 200, 1), 'area')
+# Definindo as variaveis de entrada e saida do sistema fuzzy
+idade_do_imovel = ctrl.Antecedent(np.arange(0, 200, 1), 'idade_do_imovel')
+area = ctrl.Antecedent(np.arange(12, 200, 1), 'area')
 distancia_centro = ctrl.Antecedent(np.arange(0, 20, 1), 'distancia_centro')
-valor = ctrl.Consequent(np.arange(25000, 500000, 2500), 'valor')
+valor_condominio = ctrl.Antecedent(np.arange(100, 600, 100), 'valor_condominio')
+valor = ctrl.Consequent(np.arange(25000, 1000000, 2500), 'valor')
 
-#Popula as variaveis com as funções de membership
+# Popula as variaveis com as funções de membership
 idade_do_imovel.automf(3, 'idade_do_imovel', ['low', 'mid', 'high'])
 area.automf(3, 'area', ['low', 'mid', 'high'])
 distancia_centro.automf(3, 'distancia_centro', ['low', 'mid', 'high'])
+valor_condominio.automf(3, 'valor_condominio', ['low', 'mid', 'high'])
 valor.automf(3, 'valor', ['low', 'mid', 'high'])
 
-#Definindo as variaveis de entrada do usuario
+# Definindo as variaveis de entrada do usuario
 while True:
-    idade_do_imovelInput = int(input('Digite um valor entre 0 e 80 para a idade do imovel: '))
-    if idade_do_imovelInput < 0 or idade_do_imovelInput > 80:
+    IDADE_IMOVEL_INPUT = int(
+        input('Digite um valor entre 0 e 200 para a idade do imovel: '))
+    if IDADE_IMOVEL_INPUT < 0 or IDADE_IMOVEL_INPUT > 200:
         print('\nErro: você digitou um valor muito alto ou muito baixo!\n')
     else:
         break
 
 while True:
-    areaInput = int(input('Digite um valor entre 12 e 200 para a area do imovel: '))
-    if areaInput < 12 or areaInput > 200:
+    AREA_INPUT = int(
+        input('Digite um valor entre 12 e 200 para a area do imovel: '))
+    if AREA_INPUT < 12 or AREA_INPUT > 200:
         print('\nErro: você digitou um valor muito alto ou muito baixo!\n')
     else:
         break
 
 while True:
-    distancia_centroInput = int(input('Digite um valor entre 2 e 20 para a distancia do centro: '))
-    if distancia_centroInput < 2 or distancia_centroInput > 20:
+    DISTANCIA_CENTRO_INPUT = int(
+        input('Digite um valor entre 2 e 20 para a distancia do centro: '))
+    if DISTANCIA_CENTRO_INPUT < 2 or DISTANCIA_CENTRO_INPUT > 20:
         print('\nErro: você digitou um valor muito alto ou muito baixo!\n')
     else:
         break
 
-
-#Regras
+while True:
+    VALOR_CONDOMINIO = int(
+        input('Digite um valor entre 100 e 600 para o valor do condominio: '))
+    if VALOR_CONDOMINIO < 100 or VALOR_CONDOMINIO > 600:
+        print('\nErro: você digitou um valor muito alto ou muito baixo!\n')
+    else:
+        break
+    
+# Regras
 rules = []
-rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['high'], valor['low']))
-rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['high'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['high'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['high'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['mid'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['mid'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['mid'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['high'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['high'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['high'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['mid'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['mid'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['mid'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['high'] & area['low'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['mid'] & area['low'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['low'] & valor_condominio['high'], valor['low']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['low'] & valor_condominio['mid'], valor['mid']))
+rules.append(ctrl.Rule(idade_do_imovel['low'] & area['low'] & distancia_centro['low'] & valor_condominio['low'], valor['high']))
+
+
 
 
 valorCtrl = ctrl.ControlSystem(rules)
 valorTotal = ctrl.ControlSystemSimulation(valorCtrl)
 
-
-valorTotal.input['idade_do_imovel'] = idade_do_imovelInput
-valorTotal.input['area'] = areaInput
-valorTotal.input['distancia_centro'] = distancia_centroInput
+valorTotal.input['idade_do_imovel'] = IDADE_IMOVEL_INPUT
+valorTotal.input['area'] = AREA_INPUT
+valorTotal.input['distancia_centro'] = DISTANCIA_CENTRO_INPUT
+valorTotal.input['valor_condominio'] = VALOR_CONDOMINIO
 
 valorTotal.compute()
 
 def PrintValor(val):
-    print('\nValor do imovel: R$', val, '\n')
+    print('\n Valor do imovel: R$ {:.2f}'.format(val))
 
 PrintValor(valorTotal.output['valor'])
 valor.view(sim=valorTotal)
